06e6323e9b58f94761919873c1dd4a1a36f3205c
roman.lukyanov <roman.lukyanov@x5.ru>
2024-04-13 21:07:21 +0300

    [ViHostelBot] Регистрация бота. Возможность получения сообщений

--- src/main/java/ru/vienoulis/viHostelBot/Bot.java
+++ src/main/java/ru/vienoulis/viHostelBot/Bot.java
@@ -6,21 +6,44 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Getter
 @Slf4j
 @Component
-public class Bot {
+public class Bot extends TelegramLongPollingBot {
 
     @Value("${vienoulis.telegramm.token}")
     private String botToken;
+    private final TelegramBotsApi telegramBotsApi;
+
 
     @Autowired
     public Bot() {
+        try {
+            telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        } catch (TelegramApiException e) {
+            log.error("Constructor Bot error: ", e);
+            throw new RuntimeException(e);
+        }
     }
 
     @PostConstruct
-    public void postConstruct() {
-        log.info("postConstruct: {}", this.getBotToken());
+    public void postConstruct() throws TelegramApiException {
+        telegramBotsApi.registerBot(this);
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        log.info("onUpdateReceived;");
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "Test viHostelBot";
     }
 }
